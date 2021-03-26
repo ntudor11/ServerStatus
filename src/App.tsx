4a561651a48f1e6a3fb28e417b51b422e0836177
Tudor <tn@trainaway.fit>
2021-03-26 20:54:55 +0100

    add: servers page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import logo from "./logo.svg";
+import Servers from "./pages/Servers";
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
 
-function App() {
+const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Switch>
+          <Route exact path="/" render={() => <Servers />} />
+        </Switch>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
--- src/pages/Servers.tsx
+++ src/pages/Servers.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Container, Grid, Card, Header } from "semantic-ui-react";
 
 const Servers: React.FC = () => {
-    return (
-        <div>
-            <p>a</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Container>
+        <Header as="h1">Remote Servers</Header>
+
+        <Grid columns={2} stackable>
+          <Grid.Column>
+            <Card fluid href="/server/id">
+              Abc
+            </Card>
+          </Grid.Column>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
 
 export default Servers;
