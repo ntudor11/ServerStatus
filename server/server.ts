33ffc00bccb4da3d6466066415eee2d751bdaaed
Tudor <tn@trainaway.fit>
2021-03-28 18:05:48 +0200

    add: API route for server ID

--- server/server.ts
+++ server/server.ts
@@ -30,8 +30,52 @@ app.get("/api/servers", async (req: Request, res: Response) => {
       from servers
     `
   );
+  res.send(servers);
+});
 
-  res.send(servers)
-}); 
+app.get("/api/server/:serverId", async (req: Request, res: Response) => {
+  const { serverId } = req.params;
+
+  const getServer = () => {
+    return db.task((t: any) => {
+      return t.oneOrNone(
+        `
+          select id,
+            name as "serverName",
+            server_status as "serverStatus",
+            status_time_started as "statusTimeStarted",
+            avg_uptime as "avgUptime",
+            ip as "ipAddress"
+          from servers
+          where id = $1
+        `, [serverId], (server: any) => {
+          if (server) {
+            return t.any(
+              `
+                select sm.id as "msgId",
+                  sm.status_code as "status",
+                  sm.time_stamp as "time",
+                  sm.message
+                from server_messages sm
+                join servers s
+                on s.id = sm.server_id
+                  where s.id = $1
+              `, [server.id]
+            ).then((data: any) => {
+              server.serverLog = data;
+              return server;
+            })
+          } else {
+            return { error: 'Cannot find current server'}
+          }
+        }
+      )
+    })
+  }
+
+  getServer().then((server: any) => {
+    res.send(server);
+  })
+})
 
 app.listen(PORT, () => console.log(`⚡️ Server running on port ${PORT} ⚡️`));
